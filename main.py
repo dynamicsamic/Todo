97dd702b73618703fffd88bbeb5906eaedb8ffc9
dynamicsamic <mirabyansr@gmail.com>
2024-08-21 14:46:54 +0300

    refactor: move app creation to a function, apply custom cli, drop db manipulation before and after app start

--- main.py
+++ main.py
@@ -4,63 +4,40 @@ import asyncpg
 from quart import Quart
 from quart_schema import QuartSchema, ResponseSchemaValidationError
 
-from src.data.db import init_db
+from src.commands import app_cli
+from src.data.db import get_connection_url
 from src.settings import settings
 from src.utils import AsyncpgQueryLogger
 from src.web.api import bp as api_bp
-
-app = Quart(__name__)
-app.debug = settings.DEBUG
-QuartSchema(app, convert_casing=True, conversion_preference="pydantic")
-app.register_blueprint(api_bp, url_prefix="/api/v1/todos")
 
 logger = logging.getLogger(__name__)
 query_logger = AsyncpgQueryLogger(logger)
 
 
-@app.before_serving
-async def create_db():
-    logger.info("Initializing DB")
-    await init_db(
-        settings.PG_USER,
-        settings.PG_PASSWORD,
-        settings.PG_DB,
-        settings.PG_HOST,
-        settings.PG_PORT,
-    )
-
-
-@app.before_serving
 async def create_db_pool():
     logger.info("Starting DB Connection Pool")
-    dsn = (
-        f"postgresql://{settings.PG_USER}:{settings.PG_PASSWORD}"
-        f"@{settings.PG_HOST}:{settings.PG_PORT}/{settings.PG_DB}"
-    )
     app.db_pool = await asyncpg.create_pool(
-        dsn, min_size=1, max_size=50, command_timeout=10
+        dsn=get_connection_url(), min_size=1, max_size=50, command_timeout=10
     )
 
 
-@app.after_serving
 async def close_db_pool():
     logger.info("Closing DB Connection Pool")
     await app.db_pool.close()
 
 
-@app.after_serving
-async def cleanup_db():
-    conn = await asyncpg.connect(
-        user=settings.PG_USER,
-        password=settings.PG_PASSWORD,
-        database="template1",
-        host=settings.PG_HOST,
-        port=settings.PG_PORT,
-    )
+def create_app():
+    app = Quart(__name__)
+    app = app_cli(app)
+    app.debug = settings.DEBUG
+    QuartSchema(app, convert_casing=True, conversion_preference="pydantic")
+    app.register_blueprint(api_bp, url_prefix="/api/v1/todos")
+    app.before_serving(create_db_pool)
+    app.after_serving(close_db_pool)
+    return app
 
-    with conn.query_logger(query_logger):
-        await conn.execute(f"DROP DATABASE {settings.PG_DB};")
-        await conn.close()
+
+app = create_app()
 
 
 @app.errorhandler(ResponseSchemaValidationError)
