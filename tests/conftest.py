3ef6ac07d4740c26f476cd313baa8517c16ad4db
dynamicsamic <mirabyansr@gmail.com>
2024-08-22 13:05:46 +0300

    test: fix `pytest_collection_modifyitems` incorrect doctstring and variable names. Also move global constants from deleted fixtures module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
 import pytest
 from pytest_asyncio import is_async_test
 
-# Make all fixtures and tests run in session mode.
-# Prevents asyncpg InterfaceError <class 'asyncpg.exceptions._base.InterfaceError'>:
-# cannot perform operation: another operation is in progress.
-# This happens because of pytest_asyncio runs creates differnt ivent loops for
-# each single test. Meanwhile all connections from asyncpg's connection pool
-# should run in a single event loop.
-# def pytest_collection_modifyitems(items):
-#     pytest_asyncio_tests = (item for item in items if is_async_test(item))
-#     session_scope_marker = pytest.mark.asyncio(scope="session")
-#     for async_test in pytest_asyncio_tests:
-#         async_test.add_marker(session_scope_marker, append=False)
+from src.settings import settings
+
+TODO_SAMPLE_SIZE = 1000
+TASK_SAMPLE_SIZE = 1500
+DEFAULT_LIMIT = settings.DEFAULT_PAGE_LIMIT
 
 
 def pytest_collection_modifyitems(items):
+    """
+    Make all fixtures and tests run in module scope.
+    Prevents asyncpg InterfaceError <class 'asyncpg.exceptions._base.InterfaceError'>:
+    cannot perform operation: another operation is in progress.
+    This happens because of pytest_asyncio runs creates differnt ivent loops for
+    each single test. Meanwhile all connections from asyncpg's connection pool
+    should run in a single event loop.
+    """
     pytest_asyncio_tests = (item for item in items if is_async_test(item))
-    session_scope_marker = pytest.mark.asyncio(scope="module")
+    module_scope_marker = pytest.mark.asyncio(scope="module")
     for async_test in pytest_asyncio_tests:
-        async_test.add_marker(session_scope_marker, append=False)
+        async_test.add_marker(module_scope_marker, append=False)
